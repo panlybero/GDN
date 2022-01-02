fixed masking

finetune.py
@@ -11,6 +11,7 @@ import torch.multiprocessing as mp
 import pandas as pd
 from slack_message import send_message
 import json
+import os
 def run_config(args):
     train_config = {
         'batch': args.batch,
@@ -44,7 +45,7 @@ def run_config(args):
 
     main = Main(train_config, env_config, debug=False)
     results_dict = main.run()
-    return results_dict
+    return results_dict, main.model_save_path
 
 
 class ConfigGenerator:
@@ -78,25 +79,24 @@ class ConfigGenerator:
         return len(self.all_combinations)
     
 
-
-    
 if __name__=='__main__':
 
+
     args = types.SimpleNamespace()
-    args.batch=64
-    args.device='cuda:0'
-    args.epoch = 30
+    args.batch=32
+    args.device='cuda:2'
+    args.epoch = 50
     args.slide_win=5
-    args.dim=[16,32,64,128]
+    args.dim=list(range(16,129,16))
     args.slide_stride=1
     args.dataset='swat'
     args.comment=args.dataset
-    args.random_seed=0
+    args.random_seed=np.random.randint(10000)
     args.out_layer_num = [1,2,3,4]
-    args.out_layer_inter_dim=[16,32,64,128]
-    args.decay=[0,0.1,1]
-    args.topk=[5,10,15]
-    args.n_masks=[10,20,50]
+    args.out_layer_inter_dim=list(range(16,129,16))
+    args.decay=0
+    args.topk=[5,10,15,20]
+    args.n_masks=50
     args.group_search=1
     args.model='GDN'
     args.save_path_pattern=args.dataset
@@ -104,18 +104,20 @@ if __name__=='__main__':
     args.load_model_path=''
     args.custom_edges = 'false'
     args.val_ratio = 0.2
-    args.lr = [1e-2,1e-3,1e-4]
+    args.lr = [1e-3,1e-4]
 
     torch.multiprocessing.set_start_method('spawn') 
     configs = ConfigGenerator(args)
-    
+    print(len(configs))
     random_configs = np.random.choice(range(len(configs)), 200,replace=False)
     
     random_configs = [configs[i] for i in random_configs]
-    result_list = []
-    for cfg in random_configs:
+    result_list = {}
+    for indx,cfg in enumerate(random_configs):
         
         procs = []
+        result,save_path=run_config(cfg)
+        '''
         with Pool(processes=4) as pool: 
             confs = []
             for i in range(4):
@@ -127,21 +129,30 @@ if __name__=='__main__':
                 results = pool.map_async(run_config,confs)
                 results = results.get()
             except RuntimeError:
+                print("oom error, reducing batch size to 16")
                 torch.cuda.empty_cache()
                 for conf in confs:
                     conf.batch=16
                 results = pool.map_async(run_config,confs)
                 results = results.get()
+        '''
+        #df = pd.DataFrame(results)
+        #mean = df.mean(axis=0)
+        
+        #result_dict={"params":str(cfg),**dict(mean)}        
+        result_dict={"params":str(cfg),**result}      
+        send_message(json.dumps(result_dict)) 
+        result_list[save_path]=copy.deepcopy(result_dict)
 
-            df = pd.DataFrame(results)
-            mean = df.mean(axis=0)
+        if 'finetuning_results.json' in os.listdir('./'):
+            with open('finetuning_results.json','r') as f:
+                existing_results = json.load(f)
+
+            result_list.update(existing_results)
+
+        with open('finetuning_results.json','w') as f:
+                json.dump(result_list,f)
             
-            result_dict={"params":str(cfg),**dict(mean)}           
-            send_message(json.dumps(result_dict)) 
-            result_list.append(copy.deepcopy(result_dict))
-            existing_df = pd.read_csv('finetuning_results.csv')
-            existing_df.append(pd.DataFrame(result_list))
-            existing_df.to_csv("finetuning_results.csv")
 
     send_message('Done finetuning')
 

gather_results.py
@@ -6,33 +6,36 @@ import argparse
 #n_masks, index
 
 
-
 if __name__ == '__main__':
-	
-	parser = argparse.ArgumentParser()
 
-	parser.add_argument('-m', help='masks', type = int, default=0)
-	parser.add_argument('-i', help='masks', type = int, default=0)
-	parser.add_argument('-p', help='path', type = str, default='./runs_no_edges/')
-	args=parser.parse_args()
-	print("Gathering for", args.m)
-	masks=args.m
-	path=args.p
-	idx = args.i
-	files = os.listdir(path)
-	files = list(filter(lambda x:'.txt' in x and f'masks_{masks}_{idx}' in x,files))
-	results = {'F1 ':[],'precision ':[],'recall ':[],'AUC ':[],'test_loss ':[],'val_loss ':[]}
-	print(files)
-	for f in files:
-		with open(os.path.join(path,f),'r') as f:
-			lines=f.readlines()[-6:]
-			print(lines)
-			for l in lines:
-				key,val = l.split(':')
-				val = val.strip()
-				results[key].append(float(val))
-	
-			
-	df = pd.DataFrame(results).T
-	print(df.to_csv())
-	print(df.mean(axis = 1))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', help='masks', type=int, default=0)
+    parser.add_argument('-i', help='masks', type=int, default=0)
+    parser.add_argument('-p', help='path', type=str,
+                        default='./runs_no_edges/')
+    parser.add_argument('-d', help='dataset', type=str, default='swat')
+    args = parser.parse_args()
+    print("Gathering for", args.m)
+    masks = args.m
+    path = args.p
+    idx = args.i
+    files = os.listdir(path)
+    files = list(
+        filter(lambda x: '.txt' in x and f'masks_{args.d}_{masks}_{idx}' in x, files))
+    results = {'F1 ': [], 'precision ': [], 'recall ': [],
+               'AUC ': [],'F1_loss ': [], 'precision_loss ': [], 'recall_loss ': [],
+               'AUC_loss ': [], 'test_loss ': [], 'val_loss ': []}
+    print(files)
+    for f in files:
+        with open(os.path.join(path, f), 'r') as f:
+            lines = f.readlines()[-10:]
+            print(lines)
+            for l in lines:
+                key, val = l.split(':')
+                val = val.strip()
+                results[key].append(float(val))
+
+    print(results)
+    df = pd.DataFrame(results).T
+    print(df.to_csv())
+    print(df.mean(axis=1))

main.py
@@ -69,7 +69,7 @@ class Main():
 
         self.feature_map = feature_map
 
-        
+       
 
         train_dataset_indata = construct_data(train, feature_map, labels=0)
         test_dataset_indata = construct_data(test, feature_map, labels=test.attack.tolist())
@@ -120,14 +120,16 @@ class Main():
         if train_config['n_masks']>0:
             indeces = np.arange(len(feature_map))
             perm = indeces#np.random.permutation(indeces)
-            groups = [list(indeces)]
             
-            if train_config['group_search']>0 and train_config['n_masks']!=len(feature_map):
+            
+            if train_config['n_masks']!=len(feature_map):
                 #Get groups using grouper
                 print((self.train_dataset.x.shape))
                 
                 grouper = get_feature_grouper("ClusteringGrouper",train_config=train_config,train_dataset=self.train_dataset,initial_state=perm)
                 groups = grouper.get_groups()
+            else:
+                groups =[[i] for i in indeces]
 
             self.model = MaskedGDN(train_config['n_masks'],edge_index_sets, len(feature_map), 
                     dim=train_config['dim'], 
@@ -162,7 +164,7 @@ class Main():
             model_save_path = self.env_config['load_model_path']
         else:
             model_save_path = self.get_save_path()[0]
-
+            self.model_save_path = model_save_path
             self.train_log = train(self.model, model_save_path, 
                 config = self.train_config,
                 train_dataloader=self.train_dataloader,
@@ -183,10 +185,20 @@ class Main():
         test_loss, self.test_result = test(best_model, self.test_dataloader)
         val_loss, self.val_result = test(best_model, self.val_dataloader)
 
+        test_per_entry_loss = (np.array(self.test_result[0]) - np.array(self.test_result[1]))**2
+        val_per_entry_loss = (np.array(self.val_result[0]) - np.array(self.val_result[1]))**2
 
+        anomaly_dict = dict(test = test_per_entry_loss.T, normal = val_per_entry_loss.T)
+        self.anomaly_dict = anomaly_dict
+
+        results_dict_loss=self.get_score(self.test_result, self.val_result, anomaly_scores=anomaly_dict)
+        results_dict_loss = {k+"_loss":v for k,v in results_dict_loss.items()}
         results_dict=self.get_score(self.test_result, self.val_result)
+        
+        results_dict.update(results_dict_loss)
         results_dict['val_loss']=val_loss
         results_dict['test_loss']=test_loss
+
 
         for k,v in results_dict.items():
             print(k,":",v)
@@ -227,6 +239,7 @@ class Main():
 
         if anomaly_scores is None:
             test_scores, normal_scores = get_full_err_scores(test_result, val_result)
+            print(test_scores.shape, normal_scores.shape)
         else:
             test_scores, normal_scores = anomaly_scores['test'],anomaly_scores['normal']
 
@@ -292,18 +305,18 @@ if __name__ == "__main__":
     parser.add_argument('-custom_edges', help='use_custom_edges', type = str, default='false')
     parser.add_argument('-n_masks', help='how many masks to use', type = int, default=0)
     parser.add_argument('-group_search', help='how long to search for good groups', type = float, default=0)
-    parser.add_argument('-model', help='Which model to use, GDN or Transformer', type = str, default="GDN")
+    parser.add_argument('-model', help='Which model to use, GDN or Transformer', type = str, default="Transformer")
     parser.add_argument('-lr',type=float, default=1e-3)
     args = parser.parse_args()
 
-    random.seed(args.random_seed)
-    np.random.seed(args.random_seed)
-    torch.manual_seed(args.random_seed)
-    torch.cuda.manual_seed(args.random_seed)
-    torch.cuda.manual_seed_all(args.random_seed)
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
-    os.environ['PYTHONHASHSEED'] = str(args.random_seed)
+    #random.seed(args.random_seed)
+    #np.random.seed(args.random_seed)
+    #torch.manual_seed(args.random_seed)
+    #torch.cuda.manual_seed(args.random_seed)
+    #torch.cuda.manual_seed_all(args.random_seed)
+    #torch.backends.cudnn.benchmark = False
+    #torch.backends.cudnn.deterministic = True
+    #os.environ['PYTHONHASHSEED'] = str(args.random_seed)
 
     import types
     #myargs = types.SimpleNamespace(batch=32, comment='swat', custom_edges='false', dataset='swat', decay=0, device='cuda:0', dim=64, epoch=50, group_search=1, load_model_path='', model='GDN', n_masks=50, out_layer_inter_dim=16, out_layer_num=2, random_seed=0, report='best', save_path_pattern='swat', slide_stride=1, slide_win=5, topk=15, val_ratio=0.2,lr=1e-3)

train.py
@@ -16,16 +16,11 @@ from scipy.stats import iqr
 
 
 
-
-
-
-
-
 def train(model = None, save_path = '', config={},  train_dataloader=None, val_dataloader=None, feature_map={}, test_dataloader=None, test_dataset=None, dataset_name='swat', train_dataset=None,lr=0.001):
 
     seed = config['seed']
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=config['decay'])
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=config['decay'], betas = (0.9,0.99))
 
     now = time.time()
     
@@ -43,7 +38,7 @@ def train(model = None, save_path = '', config={},  train_dataloader=None, val_d
 
     i = 0
     epoch = config['epoch']
-    early_stop_win = 15
+    early_stop_win = 10
 
     model.train()
 
@@ -69,6 +64,7 @@ def train(model = None, save_path = '', config={},  train_dataloader=None, val_d
             loss.backward()
             optimizer.step()
 
+            
             
             train_loss_list.append(loss.item())
             acu_loss += loss.item()