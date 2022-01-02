2c1726dc9ac0697ed327b3a7013e7789c85f9f29
panlybero <panliberopoulos@gmail.com>
2022-01-02 10:10:21 -0800

    script update

--- run_5_times.sh
+++ run_5_times.sh
@@ -7,7 +7,7 @@ log_save_dir=${6-"./runs_no_edges/"}
 
 for i in 0 1 2 3 
 do
-    log_save_dir_i="${log_save_dir}masks_${masks}_${runid}_$i.txt"
+    log_save_dir_i="${log_save_dir}masks_${dataset}_${masks}_${runid}_$i.txt"
     echo $log_save_dir_i
     echo $custom_edges
     bash run.sh $(($i%4)) $dataset 50 $custom_edges $i $masks $groupsearch> $log_save_dir_i &
