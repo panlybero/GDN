5df756ccb2902b07412cb3e0ab21af6218e5f5a5
panlybero <panliberopoulos@gmail.com>
2022-01-02 09:48:03 -0800

    experiment

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,10 +1,32 @@
-bash run_5_times.sh 0 0 0 &
-bash run_5_times.sh 0 0 1 
-bash run_5_times.sh 0 0 2
-#bash run_5_times.sh 10 1 0 
+bash run_5_times.sh 0 0 0 msl1 
+bash run_5_times.sh 0 0 1 msl1
+bash run_5_times.sh 0 0 2 msl1 
+bash run_5_times.sh 0 0 3 msl1
+bash run_5_times.sh 0 0 4 msl1 
+bash run_5_times.sh 0 0 5 msl1
+bash run_5_times.sh 0 0 6 msl1 
+bash run_5_times.sh 0 0 7 msl1
+bash run_5_times.sh 0 0 8 msl1 
+bash run_5_times.sh 0 0 9 msl1
+#bash run_5_times.sh 50 0 0 swat 
+#bash run_5_times.sh 50 0 1 swat
+#bash run_5_times.sh 50 0 2 swat 
+#bash run_5_times.sh 50 0 3 swat
+#bash run_5_times.sh 50 0 4 swat 
+#bash run_5_times.sh 50 0 5 swat
+#bash run_5_times.sh 50 0 6 swat 
+#bash run_5_times.sh 50 0 7 swat
+#bash run_5_times.sh 50 0 8 swat 
+#bash run_5_times.sh 50 0 9 swat
+#bash run_5_times.sh 50 0 0 swat 
+#bash run_5_times.sh 50 0 1
+#bash run_5_times.sh 50 0 2
+#bash run_5_times.sh 0 0 2
+#bash run_5_times.sh 10 1 0 & 
 #bash run_5_times.sh 10 1 1 
 #bash run_5_times.sh 10 1 2
 #bash run_5_times.sh 50 0
 #bash run_5_times.sh 10 1 0 
+#bash run_5_times.sh 50 0 0
 #bash run_5_times.sh 50 0 1
-python slack_message.py
+python slack_message.py "Done swat"
